bd4034b4a891e8469c071a61df1c0700fc71f3df
YousifA <yosif.ph2003@gmail.com>
2024-09-29 20:35:17 +0300

    swagger implemented

--- src/api/task.rs
+++ src/api/task.rs
@@ -1,10 +1,13 @@
-use actix_web::{
-    get,
-    web::Json,
-};
+use actix_web::{get, web::Json, Responder};
 
+#[utoipa::path(
+    get,
+    path = "/test",
+    responses(
+        (status = 200, description = "Task found from database", body = String)
+    )
+)]
 #[get("/test")]
-pub async fn get_task(
-) ->  Json<String> {
+pub async fn get_task() -> impl Responder {
     Json("Hello, world!".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod api;
-
+use crate::api::task::__path_get_task;
 use api::task::get_task;
-
 use actix_web::{App, HttpServer, middleware::Logger};
-
-
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,14 +11,27 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    // Derive the OpenApi struct with the properly registered path
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            get_task
+        )
+    )]
+    struct ApiDoc;
 
+    let openapi = ApiDoc::openapi();
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
             .wrap(logger)
             .service(get_task)
+            .service(SwaggerUi::new("/docs/{_:.*}").url(
+                "/api-docs/openapi.json",
+                openapi.clone(),
+            ))
     })
-        .bind(("127.0.0.1", 8080)) ?
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
