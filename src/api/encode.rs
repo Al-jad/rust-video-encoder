9fef895585e66a456910dd3f337f51caa585e585
YousifA <yosif.ph2003@gmail.com>
2024-10-07 14:53:21 +0300

    Upload to s3, enhanced compression performance

--- src/api/encode.rs
+++ src/api/encode.rs
@@ -1,3 +1,4 @@
+use std::{env, fs};
 use actix_web::{post, HttpResponse, Responder};
 use actix_multipart::Multipart;
 use uuid::Uuid;
@@ -5,7 +6,72 @@ use std::fs::File;
 use std::io::{Error, Write};
 use std::path::{Path};
 use std::process::Command;
+use aws_sdk_s3::Client;
+use aws_sdk_s3::primitives::ByteStream;
+use dotenv::dotenv;
 use futures::StreamExt;
+use tokio::io::AsyncReadExt;
+use tokio::task;
+
+struct Config {
+    s3_bucket_name: String,
+    aws_region: String,
+}
+
+impl Config {
+    fn from_env() -> Self {
+        dotenv().ok();
+
+        Config {
+            s3_bucket_name: env::var("s3_bucket_name").expect("s3_bucket_name must be set"),
+            aws_region: env::var("AWS_REGION").expect("aws_region must be set"),
+        }
+    }
+}
+
+
+
+pub struct S3Constants {
+    pub bucket_name: String,
+    pub aws_region: String,
+}
+
+pub async fn upload_folder_to_s3(folder_path: &str, file_unique_key: &str, constants: S3Constants) -> Result<String, Box<dyn std::error::Error>> {
+    let shared_config = aws_config::load_from_env().await;
+    let s3_client = Client::new(&shared_config);
+
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            continue;
+        }
+        let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+
+        let mut file = tokio::fs::File::open(&path).await?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).await?;
+
+        let s3_key = format!("videos/{}/{}", file_unique_key, file_name);
+
+        s3_client
+            .put_object()
+            .bucket(&constants.bucket_name)
+            .key(&s3_key)
+            .body(ByteStream::from(buffer))
+            .acl("public-read".parse().unwrap())
+            .send()
+            .await?;
+    }
+
+    let generated_link = format!(
+        "https://{}.s3.{}.amazonaws.com/videos/{}/master.m3u8",
+        constants.bucket_name, constants.aws_region, file_unique_key
+    );
+
+    Ok(generated_link)
+}
 
 
 
@@ -39,7 +105,7 @@ fn generate_thumbnails(uploaded_name: String) -> Result<(), String> {
     }
 }
 
-fn compress_video(uploaded_name: String) -> Result<(), String> {
+async fn compress_video(uploaded_name: String) -> Result<(), String> {
     let input_path = format!("uploads/{}/{}", uploaded_name, uploaded_name);
     if !Path::new(&input_path).exists() {
         return Err(format!("Input file not found: {}", input_path));
@@ -52,35 +118,50 @@ fn compress_video(uploaded_name: String) -> Result<(), String> {
     ];
 
 
+    let mut tasks = vec![];
     for (size_name, crf_value) in &sizes {
-        let output_path = format!("uploads/{}/{}_{}.mp4", uploaded_name, uploaded_name, size_name);
-        let output = Command::new("ffmpeg")
-            .arg("-fflags")
-            .arg("+genpts")
-            .arg("-i")
-            .arg(&input_path)
-            .arg("-analyzeduration")
-            .arg("100M")
-            .arg("-probesize")
-            .arg("50M")
-            .arg("-pix_fmt")
-            .arg("yuv420p")
-            .arg("-vcodec")
-            .arg("libx264")
-            .arg("-crf")
-            .arg(crf_value)
-            .arg(&output_path)
-            .output()
-            .map_err(|e| format!("Failed to execute ffmpeg for {} quality: {}", size_name, e))?;
+        let input_path = input_path.clone();
+        let uploaded_name = uploaded_name.clone();
+        let size_name = size_name.to_string();
+        let crf_value = crf_value.to_string();
+        tasks.push(task::spawn(async move {
+            let output_path = format!("uploads/{}/{}_{}.mp4", uploaded_name, uploaded_name, size_name);
+            let output = Command::new("ffmpeg")
+                .arg("-fflags")
+                .arg("+genpts")
+                .arg("-i")
+                .arg(&input_path)
+                .arg("-analyzeduration")
+                .arg("100M")
+                .arg("-probesize")
+                .arg("50M")
+                .arg("-pix_fmt")
+                .arg("yuv420p")
+                .arg("-vcodec")
+                .arg("libx264")
+                .arg("-crf")
+                .arg(crf_value)
+                .arg(&output_path)
+                .output()
+                .map_err(|e| format!("Failed to execute ffmpeg for {} quality: {}", size_name, e))?;
 
-        if output.status.success() {
-            println!("Video compressed successfully: {}", output_path);
-        } else {
-            return Err(format!(
-                "Error compressing video for {} quality: {}",
-                size_name,
-                String::from_utf8_lossy(&output.stderr)
-            ));
+            if output.status.success() {
+                println!("Video compressed successfully: {}", output_path);
+                Ok(())
+            } else {
+                Err(format!(
+                    "Error compressing video for {} quality: {}",
+                    size_name,
+                    String::from_utf8_lossy(&output.stderr)
+                ))
+            }
+        }));
+    }
+
+    let results = futures::future::join_all(tasks).await;
+    for result in results {
+        if let Err(e) = result.unwrap() {
+            return Err(e);
         }
     }
     create_master_playlist(&sizes, &format!("uploads/{}", uploaded_name), &uploaded_name).unwrap();
@@ -133,7 +214,7 @@ fn convert_to_hls(uploaded_name: String) -> Result<(), Box<dyn std::error::Error
             return Err(format!("Compressed video not found for {} quality: {}", size_name, input_path).into());
         }
 
-        let output = Command::new("ffmpeg")
+        Command::new("ffmpeg")
             .arg("-i")
             .arg(&input_path)
             .arg("-profile:v")
@@ -177,10 +258,7 @@ pub async fn upload_video(mut payload: Multipart) -> impl Responder {
 
     let target_directory = "uploads";
     let filename = Uuid::new_v4();
-    // create a direcotry inside the target directory with filename
-    // let target_directory = format!("{}/{}", target_directory, filename);
-    // std::fs::create_dir_all(&target_directory).expect("Failed to create directory");
-
+    let config = Config::from_env();
     while let Some(Ok(mut field)) = payload.next().await {
         let filepath = format!("{}/{}/{}", target_directory, filename, filename);
         std::fs::create_dir_all(&format!("{}/{}", target_directory, filename)).expect("Failed to create directory");
@@ -190,13 +268,13 @@ pub async fn upload_video(mut payload: Multipart) -> impl Responder {
         }
     }
 
-    match compress_video(filename.to_string()) {
-            Ok(_) => println!("Video compressed successfully"),
-            Err(e) => {
-                println!("Error compressing video: {}", e);
-                return HttpResponse::InternalServerError().finish();
-            }
+    match compress_video(filename.to_string()).await {
+        Ok(_) => println!("Video compressed successfully"),
+        Err(e) => {
+            println!("Error compressing video: {}", e);
+            return HttpResponse::InternalServerError().finish();
         }
+    }
 
     if let Err(e) = convert_to_hls(filename.to_string()) {
         eprintln!("Error: {}", e);
@@ -210,7 +288,14 @@ pub async fn upload_video(mut payload: Multipart) -> impl Responder {
         }
     }
 
-    HttpResponse::Ok().body("Video uploaded successfully!")
+    println!("S3 bucket name: {}", config.s3_bucket_name);
 
+    let link = upload_folder_to_s3(&format!("uploads/{}", filename), &filename.to_string(), S3Constants {
+        bucket_name: config.s3_bucket_name,
+        aws_region: config.aws_region,
+    }).await.unwrap();
+
+    fs::remove_dir_all(format!("uploads/{}", filename)).expect("Failed to remove directory");
+    HttpResponse::Ok().body(link)
 }
 
